05d6fe522e2933b2618faa225388054899211e3c
brainysoon <brainysoon@gmail.com>
2018-05-06 11:23:32 +0800

    [ Business ] - Fix findFirstByName to only find the first one.

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -11,7 +11,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     // 1.查询名字是*的第一个employee
-    @Query("select e from Employee e where e.name=:name")
+    @Query(value = "select e.* from Employee e where e.name=:name order by e.name limit 1", nativeQuery = true)
     Employee findFirstByName(@Param("name") String name);
 
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
