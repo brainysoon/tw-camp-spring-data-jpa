8ba900a77bbb3d5ec7e729a6470f7d22758f2ad1
brainysoon <brainysoon@gmail.com>
2018-05-06 11:15:22 +0800

    [ Business ] - Add findFirstByName query.

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -2,13 +2,17 @@ package com.example.employee.repository;
 
 import com.example.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
-    //1.查询名字是*的第一个employee
+    // 1.查询名字是*的第一个employee
+    @Query("select e from Employee e where e.name=:name")
+    Employee findFirstByName(@Param("name") String name);
 
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
 
--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -39,16 +39,18 @@ public class EmployeeJPATest {
     @Test
     public void should_return_employee_when_input_employee_name() throws Exception {
         //1.查询名字是小红的employee
-        Employee expectedEmployee = new Employee("xiaohong",19,"female",7000,1, 1);
+        Employee expectedEmployee = new Employee("xiaohong", 19, "female", 7000, 1, 1);
 
-        String actualName = null;
+
+        String actualName = employeeRepository.findFirstByName(expectedEmployee.getName()).getName();
+
         assertThat(actualName).isEqualTo(expectedEmployee.getName());
     }
 
     @Test
     public void should_return_employee_given_character_in_name_and_salary_large_than() throws Exception {
         //2.找出Employee表中第一个姓名包含`n`字符的雇员所有个人信息
-        Employee expectedEmployee = new Employee("xiaohong",19,"female",7000,1, 1);
+        Employee expectedEmployee = new Employee("xiaohong", 19, "female", 7000, 1, 1);
         String actualName = null;
         assertThat(actualName).isEqualTo(expectedEmployee.getName());
     }
@@ -56,7 +58,7 @@ public class EmployeeJPATest {
     @Test
     public void should_return_employee_name_when_employee_salary_is_max_and_given_company_id_() throws Exception {
         //3.找出一个薪资最高且公司ID是1的雇员以及该雇员的name
-        Employee expectedEmployee = new Employee("xiaohong",19,"female",7000,1, 1);
+        Employee expectedEmployee = new Employee("xiaohong", 19, "female", 7000, 1, 1);
         String actualName = null;
         assertThat(actualName).isEqualTo(expectedEmployee.getName());
     }
@@ -88,7 +90,7 @@ public class EmployeeJPATest {
     @Test
     public void should_deleted_employee_when_given_employee_name() throws Exception {
         //7.删除姓名是xiaohong的employee
-        Employee expectedEmployee = new Employee("xiaohong",19,"female",7000,1, 1);
+        Employee expectedEmployee = new Employee("xiaohong", 19, "female", 7000, 1, 1);
         Employee actualEmployee = null;
         assertThat(actualEmployee).isNull();
     }
