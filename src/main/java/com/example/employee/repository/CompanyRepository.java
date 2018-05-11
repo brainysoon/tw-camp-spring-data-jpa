2a310e7175034de4017bc15a0fee65af7524301a
brainysoon <brainysoon@gmail.com>
2018-05-11 23:24:08 +0800

    [ Business ] - Add findByPage of companyRepository.

--- src/main/java/com/example/employee/repository/CompanyRepository.java
+++ src/main/java/com/example/employee/repository/CompanyRepository.java
@@ -1,9 +1,15 @@
 package com.example.employee.repository;
 
 import com.example.employee.entity.Company;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
+
+    @Query("select c from Company c")
+    Page<Company> findByPage(Pageable pageable);
 }
--- src/test/java/com/example/employee/CompanyJPATest.java
+++ src/test/java/com/example/employee/CompanyJPATest.java
@@ -10,6 +10,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -42,5 +45,14 @@ public class CompanyJPATest {
         if (companyOption.isPresent()) actualEmployeesNumber = companyOption.get().getEmployees().size();
 
         assertThat(actualEmployeesNumber).isEqualTo(2);
+    }
+
+    @Test
+    public void should_return_page_of_companies_when_get_the_companies_by_pageable() throws Exception {
+        Pageable pageable = PageRequest.of(0, 2);
+
+        Page<Company> companyPage = companyRepository.findByPage(pageable);
+
+        assertThat(companyPage.getTotalPages()).isEqualTo(2);
     }
 }
