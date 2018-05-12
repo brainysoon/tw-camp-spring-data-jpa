0099846a1b1e0fbc117610ed25f5431d70baefe0
brainysoon <brainysoon@gmail.com>
2018-05-12 11:32:13 +0800

    [ Business ] - Add  put response wrapper.

--- src/main/java/com/example/employee/util/ResponseWrapper.java
+++ src/main/java/com/example/employee/util/ResponseWrapper.java
@@ -25,4 +25,13 @@ public class ResponseWrapper<T> {
     public static <T> Response<T> wrapResponse(ResponseInfoEnum responseInfoEnum) {
         return new Response<>(responseInfoEnum);
     }
+
+    public static <T> Response<T> wrapPutResponse(Predicate<T> updateSuccessfullyPredicate, T influenceLine) {
+
+        ResponseInfoEnum responseInfoEnum = updateSuccessfullyPredicate.test(influenceLine) ?
+                ResponseInfoEnum.RESOURCE_UPDATED : ResponseInfoEnum.RESOURCE_NOT_FOUND;
+        Response<T> response = new Response<>(responseInfoEnum);
+        response.setData(influenceLine);
+        return response;
+    }
 }
