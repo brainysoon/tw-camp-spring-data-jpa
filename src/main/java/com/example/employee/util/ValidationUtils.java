34f66cf7abc12eb3be71abd257b01b3341bad56f
brainysoon <brainysoon@gmail.com>
2018-05-12 16:01:06 +0800

    [ Business ] - Add employee adding validator.

--- src/main/java/com/example/employee/util/ValidationUtils.java
+++ src/main/java/com/example/employee/util/ValidationUtils.java
@@ -2,6 +2,7 @@ package com.example.employee.util;
 
 import com.example.employee.domain.ValidationError;
 import com.example.employee.entity.Company;
+import com.example.employee.entity.Employee;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +13,38 @@ public class ValidationUtils {
         List<ValidationError> validationErrors = new ArrayList<>();
 
         if (isEmpty(company.getCompanyName())) {
-            ValidationError validationError = new ValidationError();
-            validationError.setField("companyName");
-            validationError.setError("companyName can not be empty");
+            ValidationError validationError = new ValidationError("companyName", "companyName can not be empty");
             validationErrors.add(validationError);
         }
 
         if (!greaterThanZero(company.getEmployeesNumber())) {
-            ValidationError validationError = new ValidationError();
-            validationError.setField("employeesNumber");
-            validationError.setError("employeesNumber must be greater than zero");
+            ValidationError validationError = new ValidationError("employeesNumber", "employeesNumber must be greater than zero");
+            validationErrors.add(validationError);
+        }
+
+        return validationErrors;
+    }
+
+    public static List<ValidationError> validateEmployee(Employee employee) {
+        List<ValidationError> validationErrors = new ArrayList<>();
+
+        if (isEmpty(employee.getName())) {
+            ValidationError validationError = new ValidationError("name", "employee name must not be null");
+            validationErrors.add(validationError);
+        }
+
+        if (isEmpty(employee.getGender())) {
+            ValidationError validationError = new ValidationError("gender", "employee gender must not be null");
+            validationErrors.add(validationError);
+        }
+
+        if (!greaterThanZero(employee.getAge())) {
+            ValidationError validationError = new ValidationError("age", "employee age must greater than 0");
+            validationErrors.add(validationError);
+        }
+
+        if (!greaterThanZero(employee.getSalary())){
+            ValidationError validationError = new ValidationError("salary", "employee salary must greater than 0");
             validationErrors.add(validationError);
         }
 
